f466cf890a62d5d5e5fc308b1767238a1887233c
Pranesh Velmurugan <praneshv37@gmail.com>
2024-06-12 15:12:23 -0400

    Clean up print statements

--- KmlReader.py
+++ KmlReader.py
@@ -6,13 +6,12 @@ from pygeoif import LineString, Polygon
 def get_coordinates_from_kml(file):
     with open(file, 'rt') as file:
         doc = file.read()
-        print(doc)
         k = kml.KML()
         k.from_string(doc.encode('utf-8'))
 
         coordinates = []
         document = list(k.features())
-        parse_features(document, coordinates)
+        coordinates = parse_features(document, coordinates)
 
         return coordinates
 
@@ -21,34 +20,17 @@ def parse_features(document, coordinates):
     for feature in document:
         if isinstance(feature, kml.Placemark):
             placemark = feature
-            print("HEEERE: ", placemark)
             coordinates = parse_geometries(placemark, coordinates)
-        elif isinstance(feature, kml.Folder):
-            print("FOLDER")
-            coordinates = parse_placemarks(list(feature.features()), coordinates)
-        elif isinstance(feature, kml.Document):
-            print("DOCUMENT")
+        elif isinstance(feature, kml.Folder) or isinstance(feature, kml.Document):
             coordinates = parse_features(feature.features(), coordinates)
     return coordinates
 
 
-def parse_placemarks(features, coordinates):
-    for feature in features:
-        if isinstance(feature, kml.Placemark):
-            placemark = feature
-            coordinates = parse_geometries(placemark, coordinates)
-    return coordinates
-
-
 def parse_geometries(placemark, coordinates):
-    print("got here")
-    print(type(placemark.geometry))
-    print(isinstance(placemark.geometry, LineString))
+    # print(type(placemark.geometry))
     if isinstance(placemark.geometry, LineString):
-        print("hpm")
         coordinates.extend(placemark.geometry.coords)
     elif isinstance(placemark.geometry, Polygon):
-        print("poly?")
         coordinates.extend(placemark.geometry.exterior.coords)
     return coordinates
 
