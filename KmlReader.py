797e838577ba637f465272a595b55284ce413610
Pranesh Velmurugan <praneshv37@gmail.com>
2024-06-21 14:10:08 -0400

    Remove unused old code

--- KmlReader.py
+++ KmlReader.py
@@ -1,12 +1,10 @@
 from fastkml import kml
 from pygeoif import LineString, Polygon, MultiPolygon
-# from shapely.geometry import LineString, Polygon
 
 
 def get_coordinates_from_kml(file):
     with open(file, 'rt') as file:
         doc = file.read()
-        # print(doc)
         k = kml.KML()
         k.from_string(doc.encode('utf-8'))
 
@@ -28,7 +26,6 @@ def parse_features(document, coordinates):
 
 
 def parse_placemark(placemark, coordinates):
-    # print(type(placemark.geometry))
     if isinstance(placemark.geometry, LineString):
         coordinates.extend(placemark.geometry.coords)
     elif isinstance(placemark.geometry, Polygon):
@@ -49,20 +46,14 @@ def parse_mask_polygons(document, mask_polygons):
 
 
 def parse_mask_features(placemark, mask_polygons):
-    # print(type(placemark.geometry))
     if isinstance(placemark.geometry, MultiPolygon):
-        # print("here")
         multi_polygon = placemark.geometry
-        # print(multi_polygon.geoms[0])
         for polygon in multi_polygon.geoms:
-            # print(type(polygon))
             if isinstance(polygon, Polygon):
-                mask_polygons.append(polygon.exterior.coords) # I MADE A CHANGE HERE
+                mask_polygons.append(polygon.exterior.coords)
     elif isinstance(placemark.geometry, Polygon):
-        mask_polygons.append(placemark.geometry.exterior.coords) # AND HERE
+        mask_polygons.append(placemark.geometry.exterior.coords)
     return mask_polygons
-
-# parse_mask to parse_mask_polygon to parse_mask_polygons
 
 
 def parse_mask(file):
@@ -74,14 +65,4 @@ def parse_mask(file):
         mask_polygons = []
         document = list(k.features())
         mask_polygons = parse_mask_polygons(document, mask_polygons)
-        # mask_polygons[0].exterior.coords
         return mask_polygons
-
-
-print("HMM", get_coordinates_from_kml('/Users/pvelmuru/Desktop/IS2_RGT_0001_cycle12_23-Jun-2021.kml'))
-# print(get_coordinates_from_kml('/Users/pvelmuru/Desktop/snow_depth_mask.kml'))
-# print(get_coordinates_from_kml('/Users/pvelmuru/Downloads/Placemark.kml'))
-# print(get_coordinates_from_kml('/Users/pvelmuru/Downloads/Polygon.kml'))
-# print(parse_mask('/Users/pvelmuru/Downloads/Polygon.kml'))
-# print(parse_mask('/Users/pvelmuru/Desktop/snow_depth_mask.kml'))
-# print("yay")
--- KmlTester.py
+++ KmlTester.py
@@ -3,8 +3,6 @@ import KmlReader as Kr
 from fastkml import kml
 from pygeoif.geometry import Polygon, LineString, MultiPolygon
 
-# Create the root KML object
-
 
 def create_file(line_given):
     k = kml.KML()
@@ -65,6 +63,7 @@ def create_file_multipolygon(multi_polygon):
     print("FIND ME: ")
     print(k.to_string(prettyprint=True))
 
+
 def create_file_multiline(multi_line_string):
     k = kml.KML()
     ns = "{http://www.opengis.net/kml/2.2}"
@@ -93,36 +92,3 @@ def create_file_multiline(multi_line_string):
     f2.append(p)
     print("FIND ME: ")
     print(k.to_string(prettyprint=True))
-
-# Create a Placemark with a simple polygon geometry and add it to the
-# second folder of the Document
-# polygon = Polygon(Kr.parse_mask('/Users/pvelmuru/Desktop/snow_depth_mask.kml')[1])
-# print(polygon)
-# p = kml.Placemark(
-#     ns=ns, id="id", name="name", description="description")
-# p.geometry = polygon
-# f2.append(p)
-# print(k.to_string(prettyprint=True))
-# coordinates = Kr.parse_mask('/Users/pvelmuru/Desktop/snow_depth_mask.kml')
-
-
-# line = LineString(Intersections.test_get())
-# # print(polygon)
-# p = kml.Placemark(
-#     ns=ns, id="id", name="name", description="description")
-# p.geometry = line
-# f2.append(p)
-# print(k.to_string(prettyprint=True))
-
-# line = LineString(Kr.get_coordinates_from_kml('/Users/pvelmuru/Desktop/IS2_RGT_0001_cycle12_23-Jun-2021.kml'))
-# # print(polygon)
-# p = kml.Placemark(
-#     ns=ns, id="id", name="name", description="description")
-# p.geometry = line
-# f2.append(p)
-# print(k.to_string(prettyprint=True))
-#
-# # Conversion Test -- Looks right
-# cart_list = Conversions.gcs_list_to_cartesian(line.coords)
-# gcs_list = Conversions.cartesian_list_to_gcs(cart_list)
-# print("OK: ", gcs_list)
