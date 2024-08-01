cf47a5bc587c6d52c3b526f99a9a3a22e0992df6
Pranesh Velmurugan <praneshv37@gmail.com>
2024-08-01 12:11:39 -0400

    Modify Shp File Location

--- ShpConverter.py
+++ ShpConverter.py
@@ -2,7 +2,7 @@ import os
 import geopandas as gpd
 from fiona.drvsupport import supported_drivers
 
-# shp_folder = './Data/snow_depth_gt5cm_buffout500km_buffin400km_epsg4326'
+# shp_folder = './Public/snow_depth_gt5cm_buffout500km_buffin400km_epsg4326'
 
 supported_drivers['KML'] = 'rw'
 
@@ -10,7 +10,7 @@ supported_drivers['KML'] = 'rw'
 #     if shapefile.endswith('.shp'):
 #         print(shapefile)
 #         read_shp = gpd.read_file(shp_folder+'/'+shapefile)
-#         read_shp.to_file('./Data' + '/' + shapefile[:-4] + '.kml', driver='KML')
+#         read_shp.to_file('./Public' + '/' + shapefile[:-4] + '.kml', driver='KML')
 
 
 def shp_to_kml(shapefile):
@@ -19,13 +19,12 @@ def shp_to_kml(shapefile):
     :param shapefile: file path
     :return:
     """
-    print(shapefile)
     read_shp = gpd.read_file(shapefile)
     index = 0
-    for i in range(len(shapefile) - 1, -1, -1):
-        if shapefile[i] == '/':
-            index = i
-            break
-    new_filepath = './Data/' + shapefile[i+1:-4] + '.kml'
+    # for i in range(len(shapefile) - 1, -1, -1):
+    #     if shapefile[i] == '/':
+    #         index = i
+    #         break
+    new_filepath = os.path.join('Public', 'Mask.kml')
     read_shp.to_file(new_filepath, driver='KML')
     return new_filepath
